99111c8b988fc5a20b9235bed264c41d9e82ea48
obduber <obduber901@gmail.com>
2020-01-29 12:59:04 -0400

    third commit

---
+++ js/clase1.js
@@ -0,0 +1,10 @@
+ //ejecucion directa
+ console.log("Hola es Obduber");
+
+ //ejecucion directa
+/* if(confirm("hola")){
+     alert("si o no");
+     //ejecucion de evento
+ }
+ else{
+     alert("jajaja"); */
