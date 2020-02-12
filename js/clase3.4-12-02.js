ffa063ca5cf83636b4e9e24177734f26bb3b3399
obduber <obduber901@gmail.com>
2020-02-12 13:07:11 -0400

    clase de 12/02/20 ciclo for,let,validar

--- js/clase3.4-12-02.js
+++ js/clase3.4-12-02.js
@@ -3,6 +3,12 @@ function leer_entero(){
  cadena = prompt("ingresa el valor","50");
  if(cadena==null){
  cadena = 8;
+ }else{
+ 	cadena = parseFloat(cadena);
+ 	if (isNaN(cadena)) {
+ 		alert("debes escribir solo numeros");
+ 		leer_entero();
+ 	}
  }
 }
 leer_entero();
