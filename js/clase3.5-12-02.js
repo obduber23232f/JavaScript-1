536e0d70440c6c2794afe0f1e0cdc9bd694693a6
obduber <obduber901@gmail.com>
2020-02-12 14:37:41 -0400

    clase de 12/02/20 ciclo for,let,validar,onclick,onmouseover,funciones

--- js/clase3.5-12-02.js
+++ js/clase3.5-12-02.js
@@ -1,3 +1,9 @@
+var acum_monto = 0;
+var monto;
 function solicitar_nombre(){
-	monto = prompt("Ingrese un monto");
+	monto = parseFloat(prompt("Ingrese un monto"));
+	acum_monto = acum_monto+ monto;
+}
+function solicitar_monto(){
+	alert("El monto es :" +acum_monto);
 }
