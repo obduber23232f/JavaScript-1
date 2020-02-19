13e91ded2d9da29bdd9206ff698a7c23876c0fc9
obduber <obduber901@gmail.com>
2020-02-19 14:29:53 -0400

    Clase de 19/02/20 Jose Return,arreglos

--- js/clase4-19-02-20.js
+++ js/clase4-19-02-20.js
@@ -9,11 +9,11 @@ nombre[i]=prompt("Ingresa el Primer Nombre " +i);
  for(i=0;i<=4;i++){
 alert(nombre.sort());
  console.log("El " +i+ " Nombre es "+nombre[i]);
- }
-*/
+ }*/
 
-/*
- var notas = [];
+
+
+ /*var notas = [];
  nota = Math.random()*20;
  nota = Math.round(nota);
  var promedio = 0;
@@ -24,11 +24,15 @@ alert(nombre.sort());
  	promedio=notas[i]+promedio;
  }
  promedio=promedio/5;
- console.log("el promedio es " +promedio);
-*/
-var nombress=["jose","maria","jesus","aura"];
-function practica14_1(){
-	var nombress =["jose","maria","jesus","aura"]
+ console.log("el promedio es " +promedio);*/
+
+
+
+
+/*function practica14_1(){
+	var nombress =["jose","maria","jesus","aura"];
+	var nombress2=["a","b","c","d","e"];
+  
 	nombress.push("obduber")
 	alert("en el arreglo hay " +nombress.length+ " elementos");
 	alert(nombress.join(";"));
@@ -38,4 +42,9 @@ function practica14_1(){
 
 	nombress.pop();
 	alert(nombress.join("_"));
-}
+
+	var todo= nombress.concat(nombress2);
+	alert(todo.join(","));
+
+	todo.unshift("g","j")
+}*/
