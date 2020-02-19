859949f6d89253fa293551c580464081b102ef31
obduber <obduber901@gmail.com>
2020-02-19 13:59:26 -0400

    Clase de 19/02/20 Jose Return,arreglos

---
+++ js/clase4-19-02-20.js
@@ -0,0 +1,41 @@
+ /*var nombre=[];
+
+ for(i=0;i<=4;i++){
+nombre[i]=prompt("Ingresa el Primer Nombre " +i);
+
+ }
+
+
+ for(i=0;i<=4;i++){
+alert(nombre.sort());
+ console.log("El " +i+ " Nombre es "+nombre[i]);
+ }
+*/
+
+/*
+ var notas = [];
+ nota = Math.random()*20;
+ nota = Math.round(nota);
+ var promedio = 0;
+
+ for(i=0;i<=5;i++){
+ 	notas[i]=nota
+ 	console.log("la nota " +i+ " es: " +notas[i]);
+ 	promedio=notas[i]+promedio;
+ }
+ promedio=promedio/5;
+ console.log("el promedio es " +promedio);
+*/
+var nombress=["jose","maria","jesus","aura"];
+function practica14_1(){
+	var nombress =["jose","maria","jesus","aura"]
+	nombress.push("obduber")
+	alert("en el arreglo hay " +nombress.length+ " elementos");
+	alert(nombress.join(";"));
+
+	nombress.sort();
+	alert(nombress.join("#"));
+
+	nombress.pop();
+	alert(nombress.join("_"));
+}
